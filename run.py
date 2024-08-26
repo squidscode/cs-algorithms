22cea5f0622875533af1858a724ec658d46d7e4c
Idonotcode <siddhantmane@gmail.com>
2024-08-26 18:39:51 -0400

    run.py works

--- run.py
+++ run.py
@@ -13,15 +13,16 @@ def main():
     parser.add_argument("--function_call", help="a c function call with arguments", default=None)
     parser.add_argument("--nm_fp", help="the file path to the `nm` binary", default="nm")
     args = parser.parse_args()
+    nm_output = nm(args.nm_fp, args.shared_library)
+    c_functions = filter_c_functions(nm_output)
     if args.function_call is None:
-        nm_output = nm(args.nm_fp, args.shared_library)
-        c_functions = filter_c_functions(nm_output)
         print_shared_functions(c_functions)
-    else:
-        sl = load_shared_library(args.shared_library)
-        function_name, function_args = parse_function_call(args.function_call)
-        print(f"> {args.function_call}")
-        print(sl.__getattr__(function_name)(*function_args))
+        return
+    sl = load_shared_library(args.shared_library)
+    function_name, function_args = parse_function_call(args.function_call)
+    check_function_name(c_functions, function_name)
+    print(f"> {args.function_call}")
+    print(sl.__getattr__(function_name)(*function_args))
 
 def load_shared_library(file_name: str):
     return cdll.LoadLibrary(file_name)
@@ -37,10 +38,11 @@ def nm(nm_file_path: str, shared_library: str) -> str:
     return proc.stdout.decode("utf-8")
 
 def check_stderr(stderr: bytes) -> None:
+    perror = lambda *args, **kwargs: print(*args, file=sys.stderr, **kwargs)
     if 0 < len(stderr):
-        print("ERROR")
-        print("-----\n")
-        print(stderr.decode("utf-8"), file=sys.stderr, end="")       
+        perror("ERROR")
+        perror("-----\n")
+        perror(stderr.decode("utf-8"), end="")
         exit(1)
 
 # Given the output of the `nm -D` command, return the c bindings
@@ -53,7 +55,7 @@ def filter_c_functions(nm_output: str) -> list[str]:
     return [func[1:] for func in c_functions] # omit the "_" at the beginning of the string
 
 # Print the shared functions
-def print_shared_functions(c_functions: list[str]):
+def print_shared_functions(c_functions: list[str], print=print):
     if 0 == len(c_functions):
         print("No c function bindings found!")
         return
@@ -61,8 +63,20 @@ def print_shared_functions(c_functions: list[str]):
     for func in c_functions:
         print(f"  - {func}")
 
-def parse_function_call(function_call: str) -> tuple[str, list['ctypes.ctype']]:
-    return ("add", [12, 12])
+# TODO: Better error handling!
+# TODO: Use regex to check the validity of the function call, add hooks for different
+#       ctypes....
+def parse_function_call(function_call: str) -> tuple[str, list['ctype']]:
+    func_name = function_call.split("(")[0]
+    func_arguments = [int(i) for i in function_call.split("(")[1].split(")")[0].split(",")]
+    return func_name, func_arguments
+
+def check_function_name(allowed_funcs: list[str], func: str) -> None:
+    perror = lambda *args, **kwargs: print(*args, file=sys.stderr, **kwargs)
+    if func not in allowed_funcs:
+        perror(f"Function `{func}` is not a callable c function!")
+        print_shared_functions(allowed_funcs, print=perror)
+        exit(1)
 
 if __name__ == '__main__':
     main()
